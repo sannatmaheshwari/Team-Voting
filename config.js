ad7ba22aea8cb4bbe966a294ad444d678ad676d4
sannatmaheshwari <79201938+sannatmaheshwari@users.noreply.github.com>
2021-09-30 17:58:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,14 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+ apiKey: "AIzaSyD-y-wJ4A4wfPfte5fg6h0zABZEPpFyvxY",
+  authDomain: "voting-app-5ec8b.firebaseapp.com",
+  databaseURL: "https://voting-app-5ec8b-default-rtdb.firebaseio.com",
+  projectId: "voting-app-5ec8b",
+  storageBucket: "voting-app-5ec8b.appspot.com",
+  messagingSenderId: "537798099640",
+  appId: "1:537798099640:web:c66ce4ff60006732eeea4d",
+  measurementId: "G-9YJL834T4C
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
